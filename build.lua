5c50d6a4ad8e65e0caf747bd741b79c6b82c3be0
Lukas <oss.heindl+git@protonmail.com>
2026-06-01 21:52:33 +0200

    chore: fixup package version and l3build

--- build.lua
+++ build.lua
@@ -5,14 +5,14 @@ tagfiles = {"*.dtx", "latex.tera"}
 
 function update_tag(filename, content, tagname, tagdate)
 	if filename == "catppuccinpalette.dtx" then
-		return string.gsub(content, "\n(%%<package>\\ProvidesPackage{catppuccinpalette})%[%d%d%d%d%-%d%d%-%d%d% v%d+%.%d+%.%d+% (.*)]\n", "\n%1["..tagdate.." v"..tagname.." %2]\n")
+		return string.gsub(content, "\n(\\ProvidesPackage{catppuccinpalette})%[%d%d%d%d%-%d%d%-%d%d% v%d+%.%d+%.%d+% (.*)]\n", "\n%1["..tagdate.." v"..tagname.." %2]\n")
 	elseif filename == "beamercolorthemecatppuccin.dtx" then
-		return string.gsub(content, "\n(%%<package>\\ProvidesPackage{beamercolorthemecatppuccin})%[%d%d%d%d%-%d%d%-%d%d% v%d+%.%d+%.%d+% (.*)]\n", "\n%1["..tagdate.." v"..tagname.." %2]\n")
+		return string.gsub(content, "\n(\\ProvidesPackage{beamercolorthemecatppuccin})%[%d%d%d%d%-%d%d%-%d%d% v%d+%.%d+%.%d+% (.*)]\n", "\n%1["..tagdate.." v"..tagname.." %2]\n")
 	elseif filename == "latex.tera" then
 		local tagyear, tagmonth, tagday = tagdate:match("(%d%d%d%d)%-(%d%d)%-(%d%d)")
 		return string.gsub(
 			content,
-			"\npackageVersion: 1.2.0\nversionDate:\n    year: \"2025\"\n    month: \"07\"\n    day: \"19\"",
+			"\npackageVersion: %d+%.%d+%.%d+%\nversionDate:\n    year: \"%d%d%d%d\"\n    month: \"%d%d\"\n    day: \"%d%d\"",
 			"\npackageVersion: "..tagname.."\nversionDate:\n    year: \""..tagyear.."\"\n    month: \""..tagmonth.."\"\n    day: \""..tagday.."\""
 		)
 	end
